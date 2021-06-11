54c96c2b09c77447787f2de66a866a4181854bb6
Edney Mesquita <edney.sousa@waproject.com.br>
2021-06-10 21:12:01 -0300

    Initial commit

---
+++ Domain/Handlers/CreateCustomerHandler.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Shop.Domain.Commands.Requests;
+using Shop.Domain.Commands.Responses;
+
+namespace Shop.Domain.Handlers
+{
+    public class CreateCustomerHandler : IRequestHandler<CreateCustomerRequest, CreateCustomerResponse>
+    {
+        public Task<CreateCustomerResponse> Handle(CreateCustomerRequest request, CancellationToken cancellationToken)
+        {
+            var result = new CreateCustomerResponse
+            {
+                Id = Guid.NewGuid(),
+                Name = "Edney Mesquita",
+                Email = "edney_mesquita@outlook.com",
+                Date = DateTime.Now
+            };
+
+            return Task.FromResult(result);
+        }
+    }
+}
